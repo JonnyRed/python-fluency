71a708d262a444bc710fbd4f4e130374a8eef776
John Reynolds <jjmreynolds@gmail.com>
2023-08-08 20:17:31 +0200

    style: Format with Black
    * format with the black formatter
    * add type hints to code

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,17 +1,17 @@
-import collections
+from collections import namedtuple
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = namedtuple("Card", ["rank", "suit"])
+
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
 
-    def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits
-                                        for rank in self.ranks]
+    def __init__(self) -> None:
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._cards)
 
-    def __getitem__(self, position):
+    def __getitem__(self, position: int) -> Card:
         return self._cards[position]

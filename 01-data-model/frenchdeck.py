6c35195a09e599741b9c696bb0c9997b9d93ef39
John Reynolds <jjmreynolds@gmail.com>
2023-08-08 21:52:25 +0200

    refactor: frenchdeck according to the standards
    * Add documentation and reformat frenchdeck
    * Add main that calls `frenchdeck.doctest`
    * tidy `frenchdeck.doctest`

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,3 +1,20 @@
+"""
+This class that represents a standard 52-card deck of playing cards.
+
+The `FrenchDeck` class has the following attributes:
+
+* `ranks`: A list of the ranks of the cards in the deck.
+* `suits`: A list of the suits of the cards in the deck.
+* `_cards`: A list of all of the cards in the deck.
+
+The `FrenchDeck` class has the following methods:
+
+* `__init__()`: Initializes the `FrenchDeck` object.
+* `__len__()`: Returns the number of cards in the deck.
+* `__getitem__()`: Returns the card at the specified position in the deck.
+
+"""
+
 from collections import namedtuple
 
 Card = namedtuple("Card", ["rank", "suit"])
@@ -15,3 +32,7 @@ class FrenchDeck:
 
     def __getitem__(self, position: int) -> Card:
         return self._cards[position]
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("frenchdeck.doctest", verbose=True)

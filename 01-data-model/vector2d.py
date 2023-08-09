378b28d288636340e659ce76d691520ba12ef101
John Reynolds <jjmreynolds@gmail.com>
2023-08-09 15:49:28 +0200

    test: Add tests to vector2d.doctest
    * Added commentary and type hints to vector2d.py
    * Added __bool__ dunder to vector2d.py

--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -1,55 +1,74 @@
-"""
-vector2d.py: a simplistic class demonstrating some special methods
-
-It is simplistic for didactic reasons. It lacks proper error handling,
-especially in the ``__add__`` and ``__mul__`` methods.
-
-This example is greatly expanded later in the book.
-
-Addition::
-
-    >>> v1 = Vector(2, 4)
-    >>> v2 = Vector(2, 1)
-    >>> v1 + v2
-    Vector(4, 5)
-
-Absolute value::
-
-    >>> v = Vector(3, 4)
-    >>> abs(v)
-    5.0
-
-Scalar multiplication::
-
-    >>> v * 3
-    Vector(9, 12)
-    >>> abs(v * 3)
-    15.0
-
-"""
-
-
 import math
 
 class Vector:
+    """
+    Represents a 2D vector.
 
-    def __init__(self, x=0, y=0):
+    Attributes:
+        x (float): The x-coordinate of the vector.
+        y (float): The y-coordinate of the vector.
+    """
+
+    def __init__(self, x: float = 0.0, y: float = 0.0) -> None:
+        """
+        Initializes a Vector instance.
+
+        Args:
+            x (float, optional): The x-coordinate of the vector. Defaults to 0.
+            y (float, optional): The y-coordinate of the vector. Defaults to 0.
+        """
         self.x = x
         self.y = y
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the vector.
+
+        Returns:
+            str: The string representation of the vector.
+        """
         return f'Vector({self.x!r}, {self.y!r})'
 
-    def __abs__(self):
+    def __abs__(self) -> float:
+        """
+        Computes the magnitude (absolute value) of the vector.
+
+        Returns:
+            float: The magnitude of the vector.
+        """
         return math.hypot(self.x, self.y)
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
+        """
+        Checks if the vector is nonzero.
+
+        Returns:
+            bool: True if the vector is nonzero, False otherwise.
+        """
         return bool(abs(self))
 
-    def __add__(self, other):
+    def __add__(self, other: 'Vector') -> 'Vector':
+        """
+        Adds two vectors element-wise.
+
+        Args:
+            other (Vector): The vector to be added.
+
+        Returns:
+            Vector: The resulting vector.
+        """
         x = self.x + other.x
         y = self.y + other.y
         return Vector(x, y)
 
-    def __mul__(self, scalar):
+    def __mul__(self, scalar: float) -> 'Vector':
+        """
+        Multiplies the vector by a scalar.
+
+        Args:
+            scalar (float): The scalar multiplier.
+
+        Returns:
+            Vector: The scaled vector.
+        """
         return Vector(self.x * scalar, self.y * scalar)
